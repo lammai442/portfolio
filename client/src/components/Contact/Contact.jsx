1c0e18d418e7746a820ae830bd821aac3103cb27
Lam Mai <lam.mai442@gmail.com>
2025-07-02 13:51:52 +0200

    Working on form

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -2,13 +2,23 @@ import './Contact.css';
 function Contact() {
 	return (
 		<form action=''>
-			<label htmlFor='' className='contact__box'>
+			<label className='contact__box'>
 				If you'd like to get in touch, send me a message using the form
 				below and we'll take it from there!
 				<input
+					type='text'
 					className='contact__input'
-					placeholder='Ditt namn'></input>
+					placeholder='Namn'></input>
+				<input
+					type='email'
+					className='contact__input'
+					placeholder='Mail'></input>
+				<textarea
+					type='text'
+					className='contact__input'
+					placeholder='Meddelande'></textarea>
 			</label>
+			<button type='submit'>Skicka</button>
 		</form>
 	);
 }
