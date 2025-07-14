86630856d7d318d91a2317bbe6bbde8d49954e87
lammai442 <lam.mai442@gmail.com>
2025-07-14 17:33:42 +0200

    Added vertical time line

--- client/src/components/TimeLine/TimeLine.jsx
+++ client/src/components/TimeLine/TimeLine.jsx
@@ -1,12 +1,20 @@
 import './TimeLine.css';
-// import {
-// 	VerticalTimeline,
-// 	VerticalTimelineElement,
-// } from 'react-vertical-timeline-component';
-// import 'react-vertical-timeline-component/style.min.css';
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 function TimeLine() {
-	return <div>TimeLinse</div>;
+	return (
+		<div>
+			TimeLinsasde
+			<VerticalTimeline>
+				<VerticalTimelineElement>asdasdasdasd</VerticalTimelineElement>
+				<VerticalTimelineElement>asdasdasdasd</VerticalTimelineElement>
+			</VerticalTimeline>
+		</div>
+	);
 }
 
 export default TimeLine;
