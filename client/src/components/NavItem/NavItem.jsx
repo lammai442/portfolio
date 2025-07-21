bdcfe6ca9ee3df105618a99a34b0c4737a38220b
lammai442 <lam.mai442@gmail.com>
2025-07-21 08:31:15 +0200

    Changes on nav item

--- client/src/components/NavItem/NavItem.jsx
+++ client/src/components/NavItem/NavItem.jsx
@@ -3,7 +3,7 @@ import './NavItem.css';
 function NavItem({ onClick, text, ref }) {
 	return (
 		<li onClick={() => onClick(ref)} className='nav__item'>
-			<p className='nav__title'>{text}</p>
+			<span className='nav__title'>{text}</span>
 		</li>
 	);
 }
