6a79a7e41102b12453cbe06896be8ef417bb60d1
lammai442 <lam.mai442@gmail.com>
2025-08-13 10:18:05 +0200

    Added filter to hobbyproject

--- client/src/components/HobbyProjects/HobbyProjects.jsx
+++ client/src/components/HobbyProjects/HobbyProjects.jsx
@@ -2,23 +2,9 @@ import './HobbyProjects.css';
 import { useState } from 'react';
 
 function HobbyProjects({ url }) {
-	const [openModal, setOpenModal] = useState(false);
-
-	const handleClick = () => {
-		console.log('click');
-
-		setOpenModal(true);
-	};
-
 	return (
-		<section className='hobby-projects__box' onClick={handleClick}>
-			{openModal && <div>hejs</div>}
-			<iframe
-				src={url}
-				className='iframe'
-				allowFullScreen
-				onClick={() => handleClick}></iframe>
-			{openModal && <iframe src={url} className='iframe-modal'></iframe>}
+		<section className='hobby-projects__box'>
+			<iframe src={url} className='iframe' allowFullScreen></iframe>
 		</section>
 	);
 }
