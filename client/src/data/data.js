737d488593c80df0ab74a4cf183349d820535f88
lammai442 <lam.mai442@gmail.com>
2025-08-20 10:35:23 +0200

    Changes on data

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -10,14 +10,18 @@ import bonzAi from '../assets/webpages/bonz-ai.png';
 import catchingPokemons from '../assets/webpages/catching-pokemon.png';
 export const mobileProjects = [
 	{
-		desc: `Where it's @ är en frontend-applikation byggd i React där användare kan upptäcka och köpa biljetter till lokala musikevenemang. \n\nProjektet har utvecklats av mig från grunden. Jag har lagt fokus på att skapa en användarvänlig upplevelse med tydlig struktur och responsiv design.`,
+		desc: `Where it's @ är en frontend-applikation byggd i React där användare kan upptäcka och köpa biljetter till lokala musikevenemang. 
+		
+		Projektet har utvecklats av mig från grunden. Jag har lagt fokus på att skapa en användarvänlig upplevelse med tydlig struktur och responsiv design.`,
 		url: 'https://where-it-is-ticket-app.vercel.app/',
 		title: `Where it's @`,
 		tech: ['JavaScript', 'HTML', 'CSS', 'React', 'Figma', 'Git', 'VScode'],
 		git: 'https://github.com/lammai442/where-it-is-ticket-app',
 	},
 	{
-		desc: `Detta projekt är en mobilanpassad webbapplikation där användare kan beställa mat från foodtrucks, se uppskattad leveranstid och bläddra bland tidigare beställningar. Applikationen innehåller även en administratörsvy där det är möjligt att redigera menyn. \n\nProjektet är en del av en frontendutbildning och tillsammans med tre andra utvecklare har vi skapat appen med fokus på användarvänlighet och tydlig struktur.`,
+		desc: `Detta projekt är en mobilanpassad webbapplikation där användare kan beställa mat från foodtrucks, se uppskattad leveranstid och bläddra bland tidigare beställningar. Applikationen innehåller även en administratörsvy där det är möjligt att redigera menyn.
+		
+		Projektet är en del av en frontendutbildning och tillsammans med fyra andra utvecklare har vi skapat appen med fokus på användarvänlighet och tydlig struktur.`,
 		url: 'https://lammai442.github.io/yumyum-gimme-sum/',
 		title: 'Yum Yum Gimmie Sum',
 		tech: ['JavaScript', 'HTML', 'CSS', 'Figma', 'Git', 'Agilt', 'VScode'],
